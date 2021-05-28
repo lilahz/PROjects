9f6b710d25c937fdebb06786445087a42ed3f886
ayeletbiran <ayeletbiran3@gmail.com>
2021-05-28 14:42:40 +0300

    Junior&Project Modal Design

--- client/src/Components/HomeComponent/ProjectComponent/ProjectModalComponent.js
+++ client/src/Components/HomeComponent/ProjectComponent/ProjectModalComponent.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import { MDBBtn} from 'mdbreact';
 import Tooltip from "@material-ui/core/Tooltip";
-import Fade from '@material-ui/core/Fade';
-import {IoPersonAdd} from 'react-icons/io5'
-import {Row} from 'react-bootstrap';
+import {IoPersonAdd} from 'react-icons/io5';
 import Modal from "react-bootstrap/Modal";
-import { SocialIcon } from 'react-social-icons';
+import '../Modal.css';
 
 
 class ProjectModalComponent extends Component {
@@ -15,33 +12,40 @@ class ProjectModalComponent extends Component {
         return (
             <Modal show={this.props.isOpen} onHide={this.props.toggle} key={this.props.key}
                 aria-labelledby="contained-modal-title-vcenter"
-                centered
-                dialogClassName="modal-70w"
-                className="projectModal">
-                <Modal.Header>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    פרוייקט בשביל <b>{this.props.modalTitle}</b>
-                </Modal.Title>
-                </Modal.Header>
-                <Modal.Body style={{textAlign: "right"}}>
-                    <u>קצת עלינו</u><br/> {this.props.modalCardCompDesc}<br /><br />
-                    <u>מחפשים עזרה בתחום</u><br/>{this.props.modalField} <br />
-                    <u>קצת על הפרוייקט</u><br/>{this.props.modalCardProjectDesc}
+                centered className="modal">
+                <div className="modalTitleTop">
+                    <Modal.Title className="modalTitle" id="contained-modal-title-vcenter">
+                        פרוייקט ל{this.props.modalTitle}
+                    </Modal.Title>
+                    <div className="modalTitleUnderline"></div>
+                </div> 
+
+                <Modal.Body className="modalBody">
+                    <div>
+                        <h3 className="modalFieldTitle">מחפשים עזרה עם</h3>
+                        {this.props.modalField}<br />
+                    </div>
+                    <div>
+                        <h3 className="modalDescTitle">קצת עלינו</h3>
+                        <p className="modalDescBody">{this.props.modalCardCompDesc}<br /><br /></p>
+                    </div>
+                    <div>
+                        <h3 className="modalDescTitle">קצת על הפרוייקט</h3>
+                        <p className="modalDescBody">{this.props.modalCardProjectDesc}<br /><br /></p>
+                    </div>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Row>
-                        {/* <Tooltip title="Join Project" placement="left-start" TransitionComponent={Fade} enterDelay={100} leaveDelay={100}>
-                            <MDBBtn className="float-left col-sm-2" href="#" >
-                                <IoPersonAdd size={25}/></MDBBtn>
-                        </Tooltip> */}
-                        <div className="float-right">
-                           {this.props.modalEmail}
-                           {this.props.modalCompanyURL}
-                           {this.props.modalFacebookURL}
-                           {this.props.modalInstagramURL}
-                        </div>
-                    </Row>        
-                </Modal.Footer>
+                <div className="modalFooter">
+                    {/* <Tooltip title="Join Project" placement="left-start" TransitionComponent={Fade} enterDelay={100} leaveDelay={100}>
+                        <MDBBtn className="float-left col-sm-2" href="#" >
+                            <IoPersonAdd size={25}/></MDBBtn>
+                    </Tooltip> */}
+                    <div>
+                        {this.props.modalEmail}
+                        {this.props.modalCompanyURL}
+                        {this.props.modalFacebookURL}
+                        {this.props.modalInstagramURL}
+                    </div>
+                </div>
             </Modal>
             );
         }
