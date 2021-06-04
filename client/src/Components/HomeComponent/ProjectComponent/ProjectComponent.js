cccb1d5f7fde3e091e21100ade1adcc646f7e143
iamoronlaufman <oronln1@gmail.com>
2021-06-04 15:17:24 +0300

    cards done besides number in row

--- client/src/Components/HomeComponent/ProjectComponent/ProjectComponent.js
+++ client/src/Components/HomeComponent/ProjectComponent/ProjectComponent.js
@@ -36,54 +36,54 @@ class ProjectComponent extends Component {
     render () {
         const email_button = <SocialIcon className="SocialIcon" network="mailto" url={"mailto:" + this.props.modalEmail + "?subject=Bla"}  bgColor="#CD3636" fgColor="white"/>
         const companyURL_button = this.props.modalCompanyURL
-                                    ? <SocialIcon className="SocialIcon" url={this.props.modalCompanyURL} target="_blank" fgColor="white"/>
-                                    : null;
+            ? <SocialIcon className="SocialIcon" url={this.props.modalCompanyURL} target="_blank" fgColor="white"/>
+            : null;
         const facebookURL_button = this.props.modalFacebookURL
-                                    ? <SocialIcon className="SocialIcon" url={this.props.modalFacebookURL} target="_blank" fgColor="white"/>
-                                    : null;
+            ? <SocialIcon className="SocialIcon" url={this.props.modalFacebookURL} target="_blank" fgColor="white"/>
+            : null;
         const instagramURL_button = this.props.modalInstagramURL
-                                    ? <SocialIcon className="SocialIcon" url={this.props.modalInstagramURL} target="_blank" fgColor="white"/>
-                                    : null;
+            ? <SocialIcon className="SocialIcon" url={this.props.modalInstagramURL} target="_blank" fgColor="white"/>
+            : null;
         return (
-                <MDBCol md="4">
+            <div >
+                <MDBCol className="Item">
                     <MDBCard className="Card">
-                    <View hover zoom>
-                        <MDBCardImage className="Picture"
-                            src={this.props.cardImage 
-                                ? `data:image/jpeg;base64,${this.props.cardImage}`
-                                : "https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg"}
-                            waves />
-                        <Mask className="flex-center" overlay="white-light">
-                            <MDBBtn onClick = { this.toggle }>קצת פרטים</MDBBtn>
-                        </Mask>
-                    </View>
                         <MDBCardBody className="Body">
-                            <div className="InnerBody">
-                                <MDBCardTitle className="Title">{this.props.cardTitle}</MDBCardTitle>
-                                <MDBCardText className="Fields">{this.fieldArrayIcon(this.props.cardField).slice(0,3)}</MDBCardText>  
-                            </div>
+                            <View  hover zoom className="Picture"  >
+                                <img style={{width:"100%",height:"100%"}}
+                                    src={this.props.cardImage 
+                                        ? `data:image/jpeg;base64,${this.props.cardImage}`
+                                        : "https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg"}
+                                    waves >
+                                </img>
+                                <Mask className="InnerBody">
+                                    <MDBBtn onClick = { this.toggle }>קצת פרטים</MDBBtn>
+                                </Mask>
+                            </View>
+                            <MDBCardTitle className="Title">{this.props.cardTitle}</MDBCardTitle>
+                            <MDBCardText className="Fields">{this.fieldArrayIcon(this.props.cardField)}</MDBCardText>
                         </MDBCardBody>
                         <MDBFooter className="Footer">
-                            {email_button}
-                            {companyURL_button}
-                            {facebookURL_button}
-                            {instagramURL_button}
+                                {email_button}
+                                {companyURL_button}
+                                {facebookURL_button}
+                                {instagramURL_button}
                         </MDBFooter>
-
-                        <ProjectModalComponent className="Modal"
-                            isOpen={this.state.modal} 
-                            toggle={this.toggle}
-                            modalTitle={this.props.cardTitle}
-                            modalCardProjectDesc={this.props.cardProjectDesc}
-                            modalCardCompDesc={this.props.cardCompDesc}
-                            modalField={this.fieldArrayIcon(this.props.cardField)}
-                            modalEmail={email_button}
-                            modalCompanyURL={companyURL_button}
-                            modalFacebookURL={facebookURL_button}
-                            modalInstagramURL={instagramURL_button}
-                        />
                     </MDBCard>
+                    <ProjectModalComponent className="Modal"
+                        isOpen={this.state.modal} 
+                        toggle={this.toggle}
+                        modalTitle={this.props.cardTitle}
+                        modalCardProjectDesc={this.props.cardProjectDesc}
+                        modalCardCompDesc={this.props.cardCompDesc}
+                        modalField={this.fieldArrayIcon(this.props.cardField)}
+                        modalEmail={email_button}
+                        modalCompanyURL={companyURL_button}
+                        modalFacebookURL={facebookURL_button}
+                        modalInstagramURL={instagramURL_button}
+                    />
                 </MDBCol>
+            </div>
         )
     }
 }
